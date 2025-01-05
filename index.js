788b5525badc7f32831ae629e863feb852c5fae2
Habsyi2512 <githabsyi@gmail.com>
2025-01-05 16:34:05 +0700

    Add GraphQL type definitions and resolvers for hello query

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `#graphql
-  type Query
-`
+  type Query {
+    hello: string;
+  }
+`;
 
-const server = new ApolloServer({});
+const resolvers = {
+  Query: {
+    hello: () => {
+      return "Hello World!";
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
